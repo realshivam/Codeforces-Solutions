9b58812bda3c1c028129d422aaebbdbbcf9e9aba
realshivam <maninblack1625@gmail.com>
2020-08-02 15:58:59 +0530

    Create Watermellon.cpp

---
+++ Div 2A/Watermellon.cpp
@@ -0,0 +1,34 @@
+    #include<stdio.h>
+    #include<string.h>
+     
+    int main()
+    {
+    	int n;
+    	scanf("%d",&n);
+    	if(n==2)
+    	{
+    		printf("NO");
+    	
+    	}
+    	
+    	else
+    	{
+    		if(n%2==0)
+    		{
+    		
+    			printf("YES");
+    	
+    		}
+    	
+    		else
+    		{
+    			printf("NO");
+    	
+    		}
+    		
+    	
+    	}	
+    	
+    	return 0;
+    		
+    }
