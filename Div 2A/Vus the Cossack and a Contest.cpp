5d68cf0431f914c96e6aff3fa2d42ac1bae5fab0
realshivam <maninblack1625@gmail.com>
2020-08-04 15:35:31 +0530

    Add files via upload

---
+++ Div 2A/Vus the Cossack and a Contest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    int k;
+    cin>>k;
+
+    if(m>=n && k>=n)
+    {
+        cout<<"YES";
+    }
+
+    else{
+
+        cout<<"NO";
+    }
+
+    
+
+    return 0;
+}
